6c9ee142f9dbc6d24e53721f2f7dd56e3fcb381d
hmelnov777 <82603496+hmelnov777@users.noreply.github.com>
2021-10-11 21:44:55 +0300

    Анимация и адптация
    Была добавлена анимация и адаптация

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,19 @@
 const schemeSvg = document.querySelector(".scheme svg") 
 const totalpricetage = document.querySelector(".price-total")
+const menuButton = document.querySelector(".m-menu")
+const menu = document.querySelector(".menu")
 let cost = 11;
 let totalPrice = 0;
 schemeSvg.addEventListener('click', (event)=>{
-    if(!event.target.classList.contains('booked')){
+    if(!event.target.classList.contains('booked')&&!event.target.classList.contains('light') ){
         event.target.classList.toggle("active")
         let totalSeats = schemeSvg.querySelectorAll(".active").length
         totalPrice = totalSeats * cost;
         totalpricetage.textContent = (totalPrice)
     }
+})
+menuButton.addEventListener('click',()=>{
+    console.log("sdasd")
+    menu.classList.toggle("is-open")
+   
 })
